faa4f370da86ef6476c7c8e5eab91bbb317d45db
thoq <tristanshaw000@gmail.com>
2025-02-14 22:17:38 -0500

    fixed time not updating

--- static/js/widgets/time.js
+++ static/js/widgets/time.js
@@ -1,14 +1,15 @@
 import {militaryTime} from '../lib/settings.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const date = new Date();
     const widget = document.getElementById('timeWidget');
 
-    renderTime(widget, date);
-    setTimeout(() => renderTime(widget, date), 1000)
+    renderTime(widget);
+    setTimeout(() => renderTime(widget), 1000)
 })
 
-function renderTime(widget, date) {
+function renderTime(widget) {
+    const date = new Date();
+
     widget.innerHTML = militaryTime() ?
         `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`: date.toLocaleTimeString([], {
             hour: 'numeric',
